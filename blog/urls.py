bfbf04b86f066025acdd4f5e44e0bdc985f69dd4
Erlie Shang <erlie.shang@gmail.com>
2016-11-09 10:53:51 +0800

    Added blog's index view

---
+++ blog/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import url
+from . import views
+
+app_name = 'blog'
+urlpatterns = [
+    url(r'^$', views.IndexView.as_view(), name='index'),
+]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.views import generic
+from .models import Post
 
-# Create your views here.
+
+class IndexView(generic.ListView):
+    template_name = 'blog/index.html'
+    context_object_name = 'latest_post_list'
+
+    def get_queryset(self):
+        return Post.objects.filter(pub_date__isnull=False).order_by('-pub_date')[:5]
